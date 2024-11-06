a62ba264fbe9d5bf166bb49e92f48733347a37f2
rob <rob@mcgoye.com>
2024-11-06 20:41:13 +0000

    Cleaned up

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 require "digest/sha2"
 class ApplicationMailer < ActionMailer::Base
-  default "Message-ID"=>"#{Digest::SHA2.hexdigest(Time.now.to_i.to_s)}@#{Rails.application.credentials.smtp[:message_id_domain]}"
-  default from: Rails.application.credentials.smtp[:default_from]
+  default "Message-ID"=>"#{Digest::SHA2.hexdigest(Time.now.to_i.to_s)}@#{Rails.application.credentials.dig(:smtp, :message_id_domain)}"
+  default from: Rails.application.credentials.dig(:smtp, :default_from)
   layout "mailer"
 end
