81e0ea273d83352b0dddce2159bf7c01ce22b5e5
rob <rob@mcgoye.com>
2025-04-23 10:29:43 -0400

    updating for deployment

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module Donortrack
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.2
 
+    config.assets.initialize_on_precompile = false
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
